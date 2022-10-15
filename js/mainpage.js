60ba010db1f7fff47ca0bba79274fa1653123364
teraclitos <tefans12@gmail.com>
2022-10-14 21:43:41 -0300

    mejoro los hovers

--- js/mainpage.js
+++ js/mainpage.js
@@ -4,6 +4,14 @@ const textCiudadParque = document.getElementById("text-ciudad-parque");
 const imgCiudadParque = document.getElementById("img-ciudad-parque");
 const textTorreDual = document.getElementById("text-torre-dual");
 const imgTorreDual = document.getElementById("img-torre-dual");
+const textSantiago = document.getElementById("text-santiago");
+const imgSantiago = document.getElementById("img-santiago");
+const textCorrientes = document.getElementById("text-corrientes");
+const imgCorrientes = document.getElementById("img-corrientes");
+const textPatioDeUco = document.getElementById("text-patio-de-uco");
+const imgPatioDeUco = document.getElementById("img-patio-de-uco");
+const textTermitiere = document.getElementById("text-termitiere");
+const imgTermitiere = document.getElementById("img-termitiere");
 
 const textHoverInImg = (title) => {
   switch (title) {
@@ -26,8 +34,29 @@ const textHoverInImg = (title) => {
       imgTorreDual.style.transition = "0.5s";
 
       break;
+    case `santiago`:
+      textSantiago.style.display = "inline-block";
+      imgSantiago.style.opacity = "0.5";
+      imgSantiago.style.transition = "0.5s";
+
+      break;
+    case `corrientes`:
+      textCorrientes.style.display = "inline-block";
+      imgCorrientes.style.opacity = "0.5";
+      imgCorrientes.style.transition = "0.5s";
+
+      break;
+    case `patioDeUco`:
+      textPatioDeUco.style.display = "inline-block";
+      imgPatioDeUco.style.opacity = "0.5";
+      imgPatioDeUco.style.transition = "0.5s";
+
+      break;
 
     default:
+      textTermitiere.style.display = "inline-block";
+      imgTermitiere.style.opacity = "0.5";
+      imgTermitiere.style.transition = "0.5s";
       break;
   }
 };
@@ -52,8 +81,29 @@ const textHoverOutImg = (title) => {
       imgTorreDual.style.transition = "none";
 
       break;
+    case `santiago`:
+      textSantiago.style.display = "none";
+      imgSantiago.style.opacity = "1";
+      imgSantiago.style.transition = "none";
+
+      break;
+    case `corrientes`:
+      textCorrientes.style.display = "none";
+      imgCorrientes.style.opacity = "1";
+      imgCorrientes.style.transition = "none";
+
+      break;
+    case `patioDeUco`:
+      textPatioDeUco.style.display = "none";
+      imgPatioDeUco.style.opacity = "1";
+      imgPatioDeUco.style.transition = "none";
+
+      break;
 
     default:
+      textTermitiere.style.display = "none";
+      imgTermitiere.style.opacity = "1";
+      imgTermitiere.style.transition = "none";
       break;
   }
 };
