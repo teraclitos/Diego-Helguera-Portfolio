a0c8dc11fbdca581ea2ea5173b47499e33094080
teraclitos <tefans12@gmail.com>
2022-10-28 00:35:37 -0300

    limpio el codigo

--- js/contacto.js
+++ js/contacto.js
@@ -14,8 +14,6 @@ const RenderContact = () => {
             <div class="text-contact-container"> <label  " for="text"></label>
           <textarea autocomplete="off" placeholder="Deje su mensaje"  class="input-contact input-contact-text-narrow-screen" id="text" name="text"></textarea></div>
              <div class="btn-submit-container-narrow-screen"><input class="btn-cv" type="submit" value="enviar" /></div>`;
-
-    // RenderInputsPhone();
   } else {
     formContact.innerHTML = `  <div> <label class="label-contact" for="name">Nombre</label>
           <input autocomplete="off" class="input-contact" type="text" id="fname" name="name" /></div>
@@ -33,30 +31,3 @@ RenderContact();
 windowScreenContact.addEventListener("change", () => {
   RenderContact();
 });
-
-// const inputsContact = document.querySelectorAll(".input-contact");
-// const mainBodyContact = document.querySelector(".main-body-acercademi");
-// const navBar = document.getElementById("nat-var");
-
-// const RenderInputsPhoneFocus = () => {
-//   mainBody.classList.add("contact-modifier");
-//   navBar.style.display = "none";
-// };
-// const RenderInputsPhoneBlur = () => {
-//   mainBody.classList.remove("contact-modifier");
-//   navBar.style.display = "block";
-// };
-
-// const RenderInputsPhone = () => {
-//   if (arrowC.style.display === "inline-block") {
-//     inputsContact.forEach((input) => {
-//       input.addEventListener("focus", () => {
-//         RenderInputsPhoneFocus();
-//       });
-//       input.addEventListener("blur", () => {
-//         RenderInputsPhoneBlur();
-//       });
-//     });
-//   }
-// };
-// RenderInputsPhone();
