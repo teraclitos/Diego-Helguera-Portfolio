ab960715e47062244904f304b505a27a26c9993e
teraclitos <tefans12@gmail.com>
2022-10-27 21:32:55 -0300

    mejoro contactos

--- js/contacto.js
+++ js/contacto.js
@@ -23,7 +23,7 @@ const RenderContact = () => {
           <input autocomplete="off" class="input-contact" type="text" id="affair" name="affair" /></div>
              <div><label  class="label-contact" for="email">Email</label>
           <input autocomplete="off" class="input-contact" type="email" id="email" name="email" /></div>
-            <div class="text-contact-container"> <label class="label-contact " for="text">Deje su mensaje</label>
+            <div class="text-contact-container"> <label class="label-contact label-contact-text " for="text">Deje su mensaje</label>
           <textarea autocomplete="off" class="input-contact input-contact-text-wide-screen" id="text" name="text"></textarea></div>
              <div class="btn-submit-container-wide-screen"><input class="btn-cv" type="submit" value="enviar" /></div>`;
   }
