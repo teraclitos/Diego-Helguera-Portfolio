c9b523d40e5d0202d97c0f80a2a5152ecc6d1646
teraclitos <tefans12@gmail.com>
2022-10-28 17:09:26 -0300

    corrijo unas cosas de los media queries

--- js/contacto.js
+++ js/contacto.js
@@ -1,33 +1,34 @@
 const formContact = document.querySelector(".form-contact");
-const arrowC = document.getElementById("arrow");
 
-let windowScreenContact = window.matchMedia("(max-width: 991.98px)");
+// let windowScreenContact = window.matchMedia("(max-width: 991.98px)");
 
-const RenderContact = () => {
-  if (windowScreenContact.matches) {
-    formContact.innerHTML = `  <div> <label  for="name"></label>
-          <input autocomplete="off" placeholder="Julian"  class="input-contact" type="text" id="fname" name="name" /></div>
-            <div> <label  for="affair"></label>
-          <input autocomplete="off"  placeholder="Asunto"  class="input-contact" type="text" id="affair" name="affair" /></div>
-             <div><label for="email"></label>
-          <input autocomplete="off" placeholder="Julian@gmail.com" class="input-contact" type="email" id="email" name="email" /></div>
-            <div class="text-contact-container"> <label  " for="text"></label>
-          <textarea autocomplete="off" placeholder="Deje su mensaje"  class="input-contact input-contact-text-narrow-screen" id="text" name="text"></textarea></div>
-             <div class="btn-submit-container-narrow-screen"><input class="btn-cv" type="submit" value="enviar" /></div>`;
-  } else {
-    formContact.innerHTML = `  <div> <label class="label-contact" for="name">Nombre</label>
-          <input autocomplete="off" class="input-contact" type="text" id="fname" name="name" /></div>
-            <div> <label class="label-contact" for="affair">Asunto</label>
-          <input autocomplete="off" class="input-contact" type="text" id="affair" name="affair" /></div>
-             <div><label  class="label-contact" for="email">Email</label>
-          <input autocomplete="off" class="input-contact" type="email" id="email" name="email" /></div>
-            <div class="text-contact-container"> <label class="label-contact label-contact-text " for="text">Deje su mensaje</label>
-          <textarea autocomplete="off" class="input-contact input-contact-text-wide-screen" id="text" name="text"></textarea></div>
-             <div class="btn-submit-container-wide-screen"><input class="btn-cv" type="submit" value="enviar" /></div>`;
-  }
-};
-RenderContact();
+// const RenderContact = () => {
+//   if (windowScreenContact.matches) {
+//     formContact.innerHTML = `  <div > <label  for="name"></label>
+//           <input autocomplete="off" placeholder="Julian"  class="input-contact " type="text" id="fname" name="name" value="" />
+//           <p class="wrong-name-wide none ">El nombre debe tener al menos tres letras, no debe llevar ni números ni símbolos</p></div>
+//             <div> <label  for="asunto"></label>
+//           <input autocomplete="off"  placeholder="Asunto"  class="input-contact" type="text" id="asunto" name="asunto"  value="" /></div>
+//              <div><label for="email"></label>
+//           <input autocomplete="off" placeholder="Julian@gmail.com" class="input-contact" type="email" id="email" name="email"  value="" /></div>
+//             <div class="text-contact-container"> <label  " for="text"></label>
+//           <textarea autocomplete="off" placeholder="Deje su mensaje"  class="input-contact input-contact-text-narrow-screen" id="text" name="text"  value=""></textarea></div>
+//              <div class="btn-submit-container-narrow-screen"><input class="btn-cv" type="submit" value="enviar" /></div>`;
+//   } else {
+//     formContact.innerHTML = `  <div > <label class="label-contact" for="name">Nombre</label>
+//           <input autocomplete="off" class="input-contact " type="text" id="fname" name="name"  value="" />
+//           <p class="wrong-name-wide none ">El nombre debe tener al menos tres letras, no debe llevar ni números ni símbolos</p></div>
+//             <div> <label class="label-contact" for="asunto">Asunto</label>
+//           <input autocomplete="off" class="input-contact" type="text" id="asunto" name="asunto"  value="" /></div>
+//              <div><label  class="label-contact" for="email">Email</label>
+//           <input autocomplete="off" class="input-contact" type="email" id="email" name="email"  value="" /></div>
+//             <div class="text-contact-container"> <label class="label-contact label-contact-text " for="text">Deje su mensaje</label>
+//           <textarea autocomplete="off" class="input-contact input-contact-text-wide-screen" id="text" name="text"  value=""></textarea></div>
+//              <div class="btn-submit-container-wide-screen"><input class="btn-cv" type="submit" value="enviar" /></div>`;
+//   }
+// };
+// RenderContact();
 
-windowScreenContact.addEventListener("change", () => {
-  RenderContact();
-});
+// windowScreenContact.addEventListener("change", () => {
+//   RenderContact();
+// });
---
+++ js/formulario.js
@@ -0,0 +1,58 @@
+const form = document.getElementById("form");
+
+const inputsArray = document.querySelectorAll("#form input, textarea ");
+const wrongNameWide = document.querySelector(".wrong-name-wide");
+
+const expression = {
+  name: /^[a-zA-ZÀ-ÿ\s]{3,40}$/,
+
+  mail: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+
+  text: /\S{5,100}/,
+};
+
+const validation = (x, y) => {
+  switch (x) {
+    case "name":
+      if (expression.name.test(y) || y === "") {
+        wrongNameWide.classList.add("none");
+      } else {
+        wrongNameWide.classList.remove("none");
+      }
+
+      break;
+    case "asunto":
+      console.log("el asunto funciona");
+
+      break;
+    case "email":
+      if (expression.mail.test(y)) {
+        console.log("es valido");
+      } else {
+        console.log("No es un nombre");
+      }
+
+      break;
+
+    default:
+      if (expression.text.test(y)) {
+        console.log("es valido");
+      } else {
+        console.log("No es un nombre");
+      }
+      break;
+  }
+};
+
+inputsArray.forEach((input) => {
+  input.addEventListener("keyup", () => {
+    validation(input.name, input.value);
+  });
+  input.addEventListener("blur", () => {
+    validation(input.name, input.value);
+  });
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
