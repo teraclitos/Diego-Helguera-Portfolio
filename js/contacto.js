3243f422d59074e8ac0a31ce4467d046336e8a2e
teraclitos <tefans12@gmail.com>
2022-11-13 17:24:44 -0300

    corrijo el text area

--- js/contacto.js
+++ js/contacto.js
@@ -2,7 +2,7 @@ let windowScreenContact = window.matchMedia("(max-width: 991.98px)");
 const names = document.getElementById("name");
 const asunto = document.getElementById("asunto");
 const email = document.getElementById("email");
-const text = document.getElementById("text-area");
+const text = document.getElementById("area");
 const localSTGContact = JSON.parse(localStorage.getItem("number"));
 
 const RenderContact = () => {
