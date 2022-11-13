b320fc8427855df6035f7bdee58a76259829f911
teraclitos <tefans12@gmail.com>
2022-11-12 21:52:48 -0300

    agrego funcionalidad al boton lenguaje

--- js/mainpage.js
+++ js/mainpage.js
@@ -271,6 +271,8 @@ const projects = document.getElementById("projects");
 const me = document.getElementById("me");
 const contact = document.getElementById("contact");
 const languagePush = document.getElementById("language");
+const localSTGLanguage = JSON.parse(localStorage.getItem("number")) || [];
+const array = [];
 
 const language = () => {
   if (languagePush.innerText === "In") {
@@ -284,4 +286,27 @@ const language = () => {
     contact.innerText = "Contacto";
     languagePush.innerText = "In";
   }
+
+  localSTGLanguage.forEach((element) => {
+    array.push(element);
+  });
+  array.push("number");
+
+  localStorage.setItem("number", JSON.stringify(array));
 };
+
+const RenderNavBar = () => {
+  if (localSTGLanguage.length % 2 === 0) {
+    me.innerText = "Acerca de mí";
+    projects.innerText = "Proyectos";
+    contact.innerText = "Contacto";
+    languagePush.innerText = "In";
+  } else {
+    me.innerText = "About me";
+    projects.innerText = "Projects";
+    contact.innerText = "Contact";
+    languagePush.innerText = "Es";
+  }
+};
+
+RenderNavBar();
