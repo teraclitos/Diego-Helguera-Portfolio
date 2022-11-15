de0710e8cb00ac3553a579f5479c6fc38e3e1330
teraclitos <tefans12@gmail.com>
2022-11-14 21:20:45 -0300

    agrego el cv

--- js/acercademi.js
+++ js/acercademi.js
@@ -1,4 +1,4 @@
 const downloadCV = () => {
   console.log("descargando cv");
-  window.open("/documentation/TEMAS PARA PRIMER PARCIAL FORRAJES 2020.pdf");
+  window.open("/documentation/cv_definitivo (1).pdf");
 };
