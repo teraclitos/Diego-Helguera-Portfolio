bc78ead156881e1d0c688ab7920c982473091d2f
teraclitos <tefans12@gmail.com>
2022-11-15 21:25:48 -0300

    corrijo la fuente del tittulo

--- js/focusimg.js
+++ js/focusimg.js
@@ -1,6 +1,25 @@
+let windowScreenContact = window.matchMedia("(max-width: 991.98px)");
 const arrayImg = document.querySelectorAll(".imgs");
 const arrayImgModal = document.querySelectorAll(".imgs-modal");
 localSTGiMG = JSON.parse(localStorage.getItem("idImg")) || { id: 0 };
+
+const RenderContact = () => {
+  if (windowScreenContact.matches) {
+    arrayImg.forEach((element) => {
+      element.style.pointerEvents = "none";
+    });
+  } else {
+    arrayImg.forEach((element) => {
+      element.style.pointerEvents = "all";
+    });
+  }
+};
+RenderContact();
+
+windowScreenContact.addEventListener("change", () => {
+  RenderContact();
+});
+
 arrayImg.forEach((element, i) => {
   element.addEventListener("mousedown", () => {
     localStorage.setItem("idImg", JSON.stringify({ id: i + 1 }));
