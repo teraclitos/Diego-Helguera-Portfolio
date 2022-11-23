b1ad1f84c852cb8144183fe97a3b934c6174c4e6
teraclitos <tefans12@gmail.com>
2022-11-23 09:54:21 -0300

    agrego traduccion a santiago

--- js/language.js
+++ js/language.js
@@ -6,6 +6,8 @@ const matter = document.getElementById("matter");
 const message = document.getElementById("message");
 const submit = document.getElementById("submit-btn");
 const aboutMeText = document.querySelector(".acercademi-text");
+const textSantiago1 = document.querySelector(".text-santiago-1");
+const textSantiago2 = document.querySelector(".text-santiago-2");
 const WrongNameLanguage = document.getElementById("wrong-name");
 const wrongEmailLanguage = document.getElementById("wrong-email");
 const wrongMessageLanguage = document.getElementById("wrong-message");
@@ -24,6 +26,20 @@ const language = () => {
     contact.innerText = "Contact";
     languagePush.innerText = "ES";
     languageNarrowScreen.innerText = "Es";
+
+    if (
+      window.location.href ===
+        "https://diego-helguera.netlify.app/html/santiago.html" ||
+      window.location.href === "http://127.0.0.1:5500/html/santiago.html"
+    ) {
+      textSantiago1.innerHTML = `Casa Santiago is a single-family residential project developed in the Guillermo Cotella S.A. studio. With great lessons and achievements.
+Placed at the foot of the hill with indirect views of the city, the project goal was to design a
+modern aesthetic house, with great use of the social space that manages to take advantage of the city views and integrate the abundant natural environment with the interior space.
+  <br> <br><br>
+Team: Guillermo Cotella, Pablo Muruaga, Guadalupe Moraga, Diego Helguera. `;
+      textSantiago2.innerHTML = `As a result:
+Large openings on the ground floor, a large open terrace, an infinit pool, the kitchen as a pivot between the gallery and the living room, large wall-to-wall windows that frame the views in the bedrooms, a progressive entrance accompanied by landscaping and raw materials. `;
+    }
     if (
       window.location.href ===
         "https://diego-helguera.netlify.app/html/acercademi.html" ||
@@ -63,6 +79,17 @@ const language = () => {
     ) {
       aboutMeText.innerText = ` Arquitecto graduado de la facultad de arquitectura y urbanismo de la Universidad Nacional de Tucumán. Participé de numerosos workshops, concursos y encuentros de arquitectura.
       Con experiencia en el rubro inmobiliario y de la construcción y, a su vez, educación en el ámbito meramente proyectual; creo que ambas lógicas deben cooperar para dar como resultado proyectos económicamente viables, culturalmente representativos, socialmente responsables, ambientalmente correctos, que respondan a las necesidades y deseos personales de sus usuarios. Pero por sobre todas las cosas: una arquitectura de disfrute cotidiano, que vele por el buen vivir. `;
+    }
+    if (
+      window.location.href ===
+        "https://diego-helguera.netlify.app/html/santiago.html" ||
+      window.location.href === "http://127.0.0.1:5500/html/santiago.html"
+    ) {
+      textSantiago1.innerHTML = `  Casa Santiago fue un proyecto de vivienda unifamiliar llevado a cabo en el estudio Guillermo Cotella S.A. de grandes aprendizajes y logros.
+Emplazado al pie del cerro y con vista indirecta a la ciudad, el objetivo del proyecto fue desarrollar una vivienda de lectura moderna, de gran despliegue del espacio social que logre aprovechar las vistas a la ciudad e integrar el abundante medio natural circundante al interior.
+  <br> <br><br>
+Equipo: Guillermo Cotella, Pablo Muruaga, Guadalupe Moraga, Diego Helguera.`;
+      textSantiago2.innerHTML = ` Como resultado: Grandes aberturas en planta baja, una amplia terraza descubierta, una pileta de borde infinito, cocina como pivot de la galería y el living comedor, grandes ventanales de pared a pared que enmarcan las vistas en los dormitorios, un ingreso progresivo acompañado por paisajismo y materiales nobles.`;
     }
     if (
       window.location.href ===
@@ -114,6 +141,18 @@ const RenderLanguage = () => {
     me.innerText = "Acerca de mí";
     projects.innerText = "Proyectos";
     contact.innerText = "Contacto";
+
+    if (
+      window.location.href ===
+        "https://diego-helguera.netlify.app/html/santiago.html" ||
+      window.location.href === "http://127.0.0.1:5500/html/santiago.html"
+    ) {
+      textSantiago1.innerHTML = `  Casa Santiago fue un proyecto de vivienda unifamiliar llevado a cabo en el estudio Guillermo Cotella S.A. de grandes aprendizajes y logros.
+Emplazado al pie del cerro y con vista indirecta a la ciudad, el objetivo del proyecto fue desarrollar una vivienda de lectura moderna, de gran despliegue del espacio social que logre aprovechar las vistas a la ciudad e integrar el abundante medio natural circundante al interior.
+  <br> <br><br>
+Equipo: Guillermo Cotella, Pablo Muruaga, Guadalupe Moraga, Diego Helguera.`;
+      textSantiago2.innerHTML = ` Como resultado: Grandes aberturas en planta baja, una amplia terraza descubierta, una pileta de borde infinito, cocina como pivot de la galería y el living comedor, grandes ventanales de pared a pared que enmarcan las vistas en los dormitorios, un ingreso progresivo acompañado por paisajismo y materiales nobles.`;
+    }
     if (
       window.location.href ===
         "https://diego-helguera.netlify.app/html/acercademi.html" ||
@@ -148,6 +187,19 @@ const RenderLanguage = () => {
     me.innerText = "About me";
     projects.innerText = "Projects";
     contact.innerText = "Contact";
+    if (
+      window.location.href ===
+        "https://diego-helguera.netlify.app/html/santiago.html" ||
+      window.location.href === "http://127.0.0.1:5500/html/santiago.html"
+    ) {
+      textSantiago1.innerHTML = `Casa Santiago is a single-family residential project developed in the Guillermo Cotella S.A. studio. With great lessons and achievements.
+Placed at the foot of the hill with indirect views of the city, the project goal was to design a
+modern aesthetic house, with great use of the social space that manages to take advantage of the city views and integrate the abundant natural environment with the interior space.
+  <br> <br><br>
+Team: Guillermo Cotella, Pablo Muruaga, Guadalupe Moraga, Diego Helguera. `;
+      textSantiago2.innerHTML = `As a result:
+Large openings on the ground floor, a large open terrace, an infinit pool, the kitchen as a pivot between the gallery and the living room, large wall-to-wall windows that frame the views in the bedrooms, a progressive entrance accompanied by landscaping and raw materials. `;
+    }
     if (
       window.location.href ===
         "https://diego-helguera.netlify.app/html/acercademi.html" ||
